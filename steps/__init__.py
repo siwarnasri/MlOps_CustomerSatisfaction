1a7d98737af95ff3bffe741a65586e1977a62f21
NASRI Siwar <44530468+siwarnasri@users.noreply.github.com>
2023-10-08 08:57:07 +0100

    Update __init__.py

--- steps/__init__.py
+++ steps/__init__.py
@@ -1,1 +1,33 @@
+"""Initializer for ZenML steps.
 
+A step is a single piece or stage of a ZenML pipeline. Think of each step as
+being one of the nodes of a Directed Acyclic Graph (or DAG). Steps are
+responsible for one aspect of processing or interacting with the data /
+artifacts in the pipeline.
+
+Conceptually, a Step is a discrete and independent part of a pipeline that is
+responsible for one particular aspect of data manipulation inside a ZenML
+pipeline.
+
+Steps can be subclassed from the `BaseStep` class, or used via our `@step`
+decorator.
+"""
+
+from zenml.config import ResourceSettings
+from zenml.steps.base_parameters import BaseParameters
+from zenml.steps.base_step import BaseStep
+from zenml.new.steps.step_context import StepContext
+from zenml.steps.step_decorator import step
+from zenml.steps.step_environment import STEP_ENVIRONMENT_NAME, StepEnvironment
+from zenml.steps.step_output import Output
+
+__all__ = [
+    "step",
+    "BaseStep",
+    "BaseParameters",
+    "Output",
+    "ResourceSettings",
+    "StepContext",
+    "StepEnvironment",
+    "STEP_ENVIRONMENT_NAME",
+]
